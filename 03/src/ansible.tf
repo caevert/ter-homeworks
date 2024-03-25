ac1e32d8eb6682f75b6c96149e59f8d42dff0711
caevert <caevert@gmail.com>
2024-03-25 09:49:58 +0300

    Update ansible.tf

--- 03/src/ansible.tf
+++ 03/src/ansible.tf
@@ -1,4 +1,4 @@
-resource "local_file" "hosts_templatefile" {
+1resource "local_file" "hosts_templatefile" {
   content = templatefile("${path.module}/hosts.tftpl",
 
   { webservers = yandex_compute_instance.web, 
